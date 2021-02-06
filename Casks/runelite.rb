7300512b8651ec7dfb3c31c9e3494c886ab136a4
sharkusmanchez <sharkusmanchez@users.noreply.github.com>
2021-02-06 08:12:23 -0800

    Add runelite v2.1.5

---
+++ Casks/runelite.rb
@@ -0,0 +1,11 @@
+cask 'runelite' do
+  version '2.1.5'
+  sha256 'd8c6b4da96c6501d5529511e3cf71e0cb1e86b3bdba9ceaff9bc664bc8f76700'
+
+  url "https://github.com/runelite/launcher/releases/download/#{version}/RuneLite.dmg"
+  appcast 'https://github.com/runelite/launcher/releases.atom'
+  name 'RuneLite'
+  homepage 'https://runelite.net/'
+
+  app 'RuneLite.app'
+end
