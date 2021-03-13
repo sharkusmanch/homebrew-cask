620841a03cd56b2b2c43c4959675504dcba06f12
Marcus Sanchez <9328629+sharkusmanch@users.noreply.github.com>
2021-03-13 05:07:30 +0000

    Update ludusavi from 0.9.0 to 0.10.0 (#1)

--- Casks/ludusavi.rb
+++ Casks/ludusavi.rb
@@ -1,6 +1,6 @@
 cask "ludusavi" do
-  version "0.9.0"
-  sha256 "d1312079faf97b1e8b3d58dca6663f17dff0813a6e72b5ed396586b56f1a0407"
+  version "0.10.0"
+  sha256 "870e7ab04e534a2a2de84303b83b0b71f4c81f2c6ba1279e30a9842480849f3c"
 
   url "https://github.com/mtkennerly/ludusavi/releases/download/v#{version}/ludusavi-v#{version}-mac.zip"
   name "ludusavi"
