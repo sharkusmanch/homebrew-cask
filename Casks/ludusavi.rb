f8a6159373b40abf0e5fb8668ba38671c6c725d5
sharkusmanch <sharkusmanchez@users.noreply.github.com>
2022-12-21 15:16:51 -0800

    Update ludusavi from 0.15.0 to 0.15.2

--- Casks/ludusavi.rb
+++ Casks/ludusavi.rb
@@ -1,6 +1,6 @@
 cask "ludusavi" do
-  version "0.15.0"
-  sha256 "8671d484aad15989b95a1cc983499f66aec3529e4f09aaa9dbbe1b9f3ea071f7"
+  version "0.15.2"
+  sha256 "caf4e8e22d87db633d967df1a4a61fa60ad25183434a9ee16c1338af644c4065"
 
   url "https://github.com/mtkennerly/ludusavi/releases/download/v#{version}/ludusavi-v#{version}-mac.zip"
   name "ludusavi"
