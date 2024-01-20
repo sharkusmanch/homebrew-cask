bd18fd3e7d29053b9e3553dd31c403ce50daf0e9
Marcus Sanchez <9328629+sharkusmanch@users.noreply.github.com>
2024-01-20 13:35:43 -0800

    Create greenlight.rb

---
+++ Casks/greenlight.rb
@@ -0,0 +1,11 @@
+cask 'greenlight' do
+  version '2.0.1'
+  sha256 'ce2be509cae08467cd246da2e438ce47bdfedb3d01a6ef9884b9486dcc220f14'
+
+  url "https://github.com/unknownskl/greenlight/releases/download/v#{version}/Greenlight-#{version}-universal.dmg"
+  appcast 'https://github.com/unknownskl/greenlight/releases.atom'
+  name 'Greenlight'
+  homepage 'https://github.com/unknownskl/greenlight'
+
+  app 'Greenlight.app'
+end
