ba5312129a09ed81b0f6c1b6942b27f826e87ddf
Marcus Sanchez <9328629+sharkusmanch@users.noreply.github.com>
2024-01-20 13:37:21 -0800

    Update greenlight.rb

--- Casks/greenlight.rb
+++ Casks/greenlight.rb
@@ -3,9 +3,13 @@ cask 'greenlight' do
   sha256 'ce2be509cae08467cd246da2e438ce47bdfedb3d01a6ef9884b9486dcc220f14'
 
   url "https://github.com/unknownskl/greenlight/releases/download/v#{version}/Greenlight-#{version}-universal.dmg"
-  appcast 'https://github.com/unknownskl/greenlight/releases.atom'
   name 'Greenlight'
   homepage 'https://github.com/unknownskl/greenlight'
 
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+  
   app 'Greenlight.app'
 end
