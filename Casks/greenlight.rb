391d3059be2618b0cc9575634fdf6741b625af4b
Marcus Sanchez <9328629+sharkusmanch@users.noreply.github.com>
2024-06-21 10:52:31 -0700

    greenlight 2.3.1 (#9)
    Co-authored-by: sharkusmanch <sharkusmanch@users.noreply.github.com>

--- Casks/greenlight.rb
+++ Casks/greenlight.rb
@@ -1,15 +1,15 @@
-cask 'greenlight' do
-  version '2.0.1'
-  sha256 'ce2be509cae08467cd246da2e438ce47bdfedb3d01a6ef9884b9486dcc220f14'
+cask "greenlight" do
+  version "2.3.1"
+  sha256 "8ee2bd65cdeeb3a5af772e5f4b5d6d47f80e48a04db3fd276fc67b70e573452b"
 
   url "https://github.com/unknownskl/greenlight/releases/download/v#{version}/Greenlight-#{version}-universal.dmg"
-  name 'Greenlight'
-  homepage 'https://github.com/unknownskl/greenlight'
+  name "Greenlight"
+  homepage "https://github.com/unknownskl/greenlight"
 
   livecheck do
     url :url
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
-  
-  app 'Greenlight.app'
+
+  app "Greenlight.app"
 end
