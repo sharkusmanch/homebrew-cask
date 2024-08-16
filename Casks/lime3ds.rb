ef5640642af7a289e2324c6dde5e84b7e3057c1b
Marcus Sanchez <9328629+sharkusmanch@users.noreply.github.com>
2024-08-15 22:01:26 -0700

    lime3ds 2117 (#11)
    Co-authored-by: Marcus Sanchez <sharkusmanch@users.noreply.github.com>

--- Casks/lime3ds.rb
+++ Casks/lime3ds.rb
@@ -1,10 +1,10 @@
 cask "lime3ds" do
-  version "2116"
-  sha256 "a406ae3df6ca7408d7f56597969af68d6cd31f3d41dfc49e887c4ce79030e137"
+  version "2117"
+  sha256 "fb72d625b2a3111a63078a8ea6eb92eb705c634924f600f45f874389a1c1eefc"
 
   url "https://github.com/Lime3DS/Lime3DS/releases/download/#{version}/lime3ds-#{version}-macos-universal.tar.gz"
   name "lime3ds"
-  desc "A Nintendo 3DS emulator based on Citra"
+  desc "Nintendo 3DS emulator based on Citra"
   homepage "https://github.com/Lime3DS/Lime3DS"
 
   livecheck do
@@ -12,9 +12,7 @@ cask "lime3ds" do
     regex(/^?(\d+)$/i)
   end
 
-  zap trash: [
-    "~/Library/Application Support/Lime3DS"
-  ]
-
   app "lime3ds-#{version}-macos-universal/lime3ds-gui.app", target: "Lime3DS.app"
+
+  zap trash: "~/Library/Application Support/Lime3DS"
 end
