b35fd65d67ffdfbd13ed6b306754ece6cf8f85e9
Marcus Sanchez <9328629+sharkusmanch@users.noreply.github.com>
2025-03-29 09:59:52 -0700

    Update azahar.rb

--- Casks/azahar.rb
+++ Casks/azahar.rb
@@ -2,7 +2,7 @@ cask "azahar" do
   version "2120.1"
   sha256 "1bf1f23ba8033dd035c4dbe7209e7be887bd20c92b61263c951060463a2d2dc1"
 
-  url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.tar.gz"
+  url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.zip"
   name "azahar"
   desc "An open-source 3DS emulator project based on Citra."
   homepage "https://github.com/Lime3DS/Lime3DS"
